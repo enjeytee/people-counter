600e2527d08b617882f308aa4d0851ea704e8a52
enjeytee <enjeytee@gmail.com>
2022-09-02 20:21:41 +0800

    af

---
+++ index.js
@@ -0,0 +1,15 @@
+let count = 0
+
+function increment()
+{
+    count += 1
+    document.getElementById("count-el").textContent = count
+}
+
+function save()
+{
+    document.getElementById("save-el").textContent += count + ", "
+    count = 0
+    document.getElementById("count-el").textContent = count
+}
+
